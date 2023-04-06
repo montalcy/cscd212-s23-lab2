0221406b2a1481b8f77995bd56793a3133b307fd
Cynthia Montalvo <cmontalvo@ewu.edu>
2023-04-06 10:45:21 -0700

    last_edit

--- src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
+++ src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
@@ -7,6 +7,8 @@ import java.util.Comparator;
 public class TelevisionResolutionMakeDescendingComparator implements Comparator<Television> {
     @Override
     public int compare(Television t1, Television t2) {
+        if(t1==null|t2==null)
+            throw new IllegalArgumentException("null parameter in TelevisionResolutionMakeDescendingComparator");
         if(t1.getResolution()-(t2.getResolution())==0){
             return t1.getMake().compareTo(t2.getMake());
         }
--- src/cscd212comparators/lab2/TelevisionScreenSizeComparator.java
+++ src/cscd212comparators/lab2/TelevisionScreenSizeComparator.java
@@ -8,6 +8,8 @@ public class TelevisionScreenSizeComparator implements Comparator<Television> {
 
     @Override
     public int compare(Television t1, Television t2) {
+        if(t1==null||t2==null)
+            throw new IllegalArgumentException("null parameter in TelevisionScreenSizeComparator");
         return t1.getScreenSize()-t2.getScreenSize();
     }
 }
