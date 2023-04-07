d7a5ebc660dd4d9a1905557814d2a3c86fbb0f7f
Cynthia Montalvo <cmontalvo@ewu.edu>
2023-04-07 10:33:20 -0700

    final submission

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -11,8 +11,8 @@ public class Television implements Comparable<Television>{
     private final boolean smart;
 
     public Television(final String make, final String model, final  boolean smart, final int screenSize,final int resolution){
-        if(make.isBlank()||model.isBlank()||make==null||model==null||screenSize<32||resolution<720)
-            throw new IllegalArgumentException("Please check input");
+        if(make==null||model==null||screenSize<32||resolution<720)
+            throw new IllegalArgumentException("Invalid parameter in constructor");
         if(resolution==2160)
             this.fourK=true;
         else
@@ -44,36 +44,42 @@ public class Television implements Comparable<Television>{
     }
     @Override
     public String toString(){
-        String hold=this.getMake()+","+this.getModel()+" inch";
-        if(fourK==true)
-            hold=hold+" smart tv with 4K resolution";
+        String smart;
+        if(this.smart==true)
+            smart=" smart";
         else
-            hold=hold+" tv with "+resolution+"resolution";
-        return hold;
+            smart="";
+        String four;
+        if(this.fourK==true)
+            four="4K";
+        else
+            four=""+this.resolution;
+        return this.getMake()+"-"+this.getModel()+", "+this.getScreenSize()+" inch"+smart+" tv with "+four+" resolution";
     }
     @Override
-    public boolean equals(Object o){
+    public boolean equals(final Object o){
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Television tv = (Television) o;
-        return Objects.equals(make, tv.getMake())&& Objects.equals( model, tv.getModel())&&Objects.equals(smart, tv.smart)&&Objects.equals(screenSize, tv.getScreenSize())&&Objects.equals(resolution, tv.getResolution());
+        return Objects.equals(this.make, tv.getMake())&& Objects.equals( this.model, tv.getModel())&&Objects.equals(this.smart, tv.smart)&&Objects.equals(this.screenSize, tv.getScreenSize())&&Objects.equals(this.resolution, tv.getResolution());
     }
 
     @Override
     public int hashCode(){
-        return Objects.hash(getMake(), getModel(), this.smart, getScreenSize(), getResolution());
+        return Objects.hash(this.getMake(), this.getModel(), this.smart, this.getScreenSize(), this.getResolution());
     }
+
     @Override
-    public int compareTo(Television another){
+    public int compareTo(final Television another){
         if(another==null)
-            throw new IllegalArgumentException("Illegal arg for compareTo");
+            throw new IllegalArgumentException("null parameter in the compareTo method");
         if(this.getMake().compareTo(another.getMake())==0){
             if(this.getModel().compareTo(another.getModel())==0){
                 return this.getScreenSize()-(another.getScreenSize());
             }
             return this.getModel().compareTo(another.getModel());
         }
-        return this.getModel().compareTo(another.getModel());
+        return this.getMake().compareTo(another.getMake());
     }
 
 }
--- src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
+++ src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
@@ -6,12 +6,12 @@ import java.util.Comparator;
 
 public class TelevisionResolutionMakeDescendingComparator implements Comparator<Television> {
     @Override
-    public int compare(Television t1, Television t2) {
+    public int compare(final Television t1, final Television t2) {
         if(t1==null|t2==null)
             throw new IllegalArgumentException("null parameter in TelevisionResolutionMakeDescendingComparator");
-        if(t1.getResolution()-(t2.getResolution())==0){
-            return t1.getMake().compareTo(t2.getMake());
+        if(t2.getResolution()-(t1.getResolution())==0){
+            return t2.getMake().compareTo(t1.getMake());
         }
-        return t1.getResolution()-(t2.getResolution());
+        return t2.getResolution()-(t1.getResolution());
     }
 }
